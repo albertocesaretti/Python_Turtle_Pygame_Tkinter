65f91550db918dc5770a12cf033f77cba1875bb3
albertocesaretti <albertocesaretti@gmail.com>
2025-03-30 17:41:59 +0200

    30-03-25
    30-03-25 cancello i singoli file

--- modulo1.py
+++
@@ -1,8 +0,0 @@
-def mia_funzione():
-  print("Funzione eseguita")
-
-if __name__ == "__main__":
-  # Questo codice viene eseguito solo se il modulo viene eseguito come programma principale
-  mia_funzione()
-  print("Modulo eseguito come programma principale")
-  print(__name__)
--- test.py
+++
@@ -1,5 +0,0 @@
-import modulo1
-
-print("ilmodulo 1 non viene eseguito come programma ma è importato")
-#modulo1.mia_funzione()
-print(modulo1)
